61ad3f3effea92aa2035c7d1769872d627798e78
Kushagra-0 <kushagrashukla.dev@gmail.com>
2024-08-07 16:35:33 +0530

    Made minor improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/card/card";
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -23,37 +24,33 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Main content */}
         <div className="absolute z-10 text-center mx-80">
           <p className="text-9xl font-bold text-[#1ee07f]">Mortgages</p>
           <p className="text-9xl font-bold text-[#1ee07f] mb-72">made simple</p>
         </div>
 
-        {/* Additional text on the right */}
         <div className="relative z-10 text-right text-white mt-72 mr-64">
-          <img 
+          <Image
             src="review.png"
             className="h-auto w-52"
+            alt="rating"
           />
           <p className="text-sm">4.6 Stars | 3177 Google Reviews</p>
         </div>
 
-        {/* Image at the bottom */}
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-          <img 
+          <Image
             src="hand.webp" 
             alt="Background Image"
-            className="w-[450px] h-auto" // Adjust size as needed
+            className="w-[450px] h-auto" 
           />
         </div>
       </section>
 
       <section id="qa" className="min-h-screen pt-32 pb-32 bg-white flex">
-        {/* Left portion with Card component */}
         <div className="flex-1 flex items-center justify-center">
           <Card />
         </div>
-        {/* Right portion with a button */}
         <div className="flex-1 flex flex-col items-start justify-center">
           <div className="text-8xl text-[#292B29]">Find out why</div>
           <div className="text-8xl text-[#292B29]">we're better.</div>
